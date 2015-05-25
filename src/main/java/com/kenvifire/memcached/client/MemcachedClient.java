238040a193729c6101feb82f3b23712bc495cab6
kenvi <mrwhite@163.com>
2015-05-25 23:16:02 +0800

    first commit

--- src/main/java/com/kenvifire/memcached/client/MemcachedClient.java
+++ src/main/java/com/kenvifire/memcached/client/MemcachedClient.java
@@ -1,7 +1,76 @@
 package com.kenvifire.memcached.client;
 
+import org.apache.mina.core.future.CloseFuture;
+import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.transport.socket.SocketConnector;
+import org.apache.mina.transport.socket.nio.NioSocketConnector;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.InetSocketAddress;
+
 /**
  * Created by hannahzhang on 15/5/25.
  */
 public class MemcachedClient {
+    private SocketConnector connector;
+    private ConnectFuture future;
+    private IoSession session;
+
+
+    public static void main(String[] args) throws Exception{
+        if (args.length != 2) {
+            System.out.println(MemcachedClient.class.getName() + " <hostname> <port>");
+        }
+
+        MemcachedClient client = new MemcachedClient();
+        client.connect("182.92.77.50",11211);
+
+        client.store("test","111",1,300);
+
+    }
+
+    public boolean connect(String host, int port){
+        connector = new NioSocketConnector();
+        connector.setConnectTimeoutMillis(30 * 1000);
+
+        future = connector.connect(new InetSocketAddress(host,port));
+
+        future.awaitUninterruptibly();
+
+        session = future.getSession();
+
+        return true;
+    }
+
+    public boolean store(String key,Serializable value,int flags, long exptime) throws Exception{
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream outputStream = new ObjectOutputStream(bos);
+
+        long length = bos.size();
+
+        outputStream.close();
+
+        String msg = String.format("set %s %s %s %s\r\n",key,flags,exptime,length);
+
+        session.write(msg);
+        session.write(value);
+        return true;
+    }
+
+
+    public boolean quit(){
+        //TODO
+        CloseFuture closeFuture = session.getCloseFuture();
+        future.awaitUninterruptibly();
+        connector.dispose();
+        return true;
+    }
+
+
+
+
 }
--- src/main/java/com/kenvifire/memcached/client/MemcachedCommand.java
+++ src/main/java/com/kenvifire/memcached/client/MemcachedCommand.java
@@ -1,7 +1,30 @@
 package com.kenvifire.memcached.client;
 
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by hannahzhang on 15/5/25.
  */
 public enum MemcachedCommand {
+    SET("set"),
+    ADD("add"),
+    REPLACE("replace"),
+    APPEND("append"),
+    PREPEND("prepend");
+
+    private String command;
+
+    private MemcachedCommand(String command){
+        this.command = command;
+    }
+
+    public static MemcachedCommand parse(String command){
+        for(MemcachedCommand commandEnum : values()){
+            if(StringUtils.equals(command,commandEnum.command)){
+                return commandEnum;
+            }
+        }
+        return null;
+    }
 }
